8f36f38eda9fa9b2ccb15c447a72d168855a1a38
sgyorkos <simon.gyorkos@gmail.com>
2018-11-25 07:47:55 -0500

    connected backend to frontend

--- src/catalog.js
+++ src/catalog.js
@@ -1,12 +1,50 @@
 //var vin, year, make, model, body_type, drive_type, coi, styleid, price, engine_type, fuel_type, hp, engine_size, fe_cty, fe_hwy, fuel_cap, transmission, tire_size, interior, vObj, compressed;
 var year, make, model, bodyType, driveType, styleID, price, subdivision, marketClass, vObj, compressed;
+var vObj = new Map();
 
+function createTable() {
+    var table = document.getElementById("catalogresponse");
+       //console.log("DM row 0.0: " + table.rows.length);
+    // clearTable(table);
 
+    var row = table.insertRow(table.rows.length);
+    row.insertCell(0).innerHTML = "Year";
+    row.insertCell(1).innerHTML = vObj.get("Year");
+    row = table.insertRow(table.rows.length);
+    row.insertCell(0).innerHTML = "Make";
+    row.insertCell(1).innerHTML = vObj.get("Make");
+    row = table.insertRow(table.rows.length);
+    row.insertCell(0).innerHTML = "Model";
+    row.insertCell(1).innerHTML = vObj.get("Model");
+
+    table = document.getElementById("cr");
+    var titles = vObj.get("Styles");
+    row = table.insertRow(table.rows.length);
+    var i;
+    for (i = 0; i < titles.length; i++) {
+      row.insertCell(i).innerHTML = titles[i];
+    }
+    var objs = vObj.get("Styles");
+    for (i = 0; i < objs.length; i++) {
+      row = table.insertRow(table.rows.length);
+      var j;
+      for (j = 0; j < titles.length; j++) {
+        row.insertCell(j).innerHTML = objs.get(titles[j]);
+      }
+    }
+};
+
+function clearTable(table) {
+  console.log("DM row 3: " + JSON.stringify(table));
+  var rowCount = table.rows.length;
+  while(rowCount && --rowCount >= 0)
+    table.deleteRow(rowCount);
+}
 
 function generateCatalog() {
-   var year = 2018//$("#year").val();
-   var make = "Toyota"//$("#make").val();
-   var model = "Sienna"//$("#model").val();
+   var year = $("#year").val();
+   var make = $("#make").val();
+   var model = $("#model").val();
    var url = "https://us-central1-autolopedia.cloudfunctions.net/getCatalog/?year="+year+"&make="+make+"&model="+model;
 
    $.ajax({
@@ -21,34 +59,64 @@ function generateCatalog() {
             //vin = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.vin;
             //console.log(vin);
             year = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].$.modelYear;
-            console.log(year);
+            //vObj.set("Year", year);
+            //console.log(year);
             make = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].$.bestMakeName;
-            console.log(make);
+            //vObj.set("Make", make);
+            //console.log(make);
             model = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].$.bestModelName;
-            console.log(model);
+            //vObj.set("Model", make);
+            //console.log(model);
             var len = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style.length;
+            var table = document.getElementById("catalogresponse");
+            var row = table.insertRow(table.rows.length);
+            row.insertCell(0).innerHTML = "Year";
+            row.insertCell(1).innerHTML = year;
+            row = table.insertRow(table.rows.length);
+            row.insertCell(0).innerHTML = "Make";
+            row.insertCell(1).innerHTML = make;
+            row = table.insertRow(table.rows.length);
+            row.insertCell(0).innerHTML = "Model";
+            row.insertCell(1).innerHTML = model;
+
+
+            table = document.getElementById("cr");
+            var titles = ['Body Type', 'Drive Type', 'Style ID', 'Price', 'Subdivision', 'Market Class'];
+            row = table.insertRow(table.rows.length);
+            var i;
+            for (i = 0; i < titles.length; i++) {
+              row.insertCell(i).innerHTML = titles[i];
+            }
             var styles = [];
             console.log(len);
             var i = 0;
             console.log(i);
             while (i < len) {
+              row = table.insertRow(table.rows.length);
               bodyType = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].bodyType[0]['_'];
               console.log(bodyType);
+              row.insertCell(0).innerHTML = bodyType;
               driveType = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].$.drivetrain;
               console.log(driveType);
+              row.insertCell(1).innerHTML = driveType;
               styleID = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].$.id;
               console.log(styleID);
+              row.insertCell(2).innerHTML = styleID;
               price = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].basePrice[0].$.msrp;
               console.log(price);
+              row.insertCell(3).innerHTML = price;
               subdivision = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].subdivision[0]['_'];
               console.log(subdivision);
+              row.insertCell(4).innerHTML = subdivision;
               marketClass = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[i].marketClass[0]['_'];
               console.log(marketClass);
+              row.insertCell(5).innerHTML = marketClass;
               styles.push(Style(bodyType, driveType, styleID, price, subdivision, marketClass));
               i += 1;
             }
-
-            vObj = Catalog(year, make, model, styles);
+            //vObj.set("Styles", styles);
+            console.log(styles);
+            //vObj = Catalog(year, make, model, styles);
 
             compressed = JSON.stringify(vObj);
 
@@ -56,7 +124,7 @@ function generateCatalog() {
        }
    });
 
-
+   //createTable();
 };
 
 function Style(bodyType, driveType, styleID, price, subdivision, marketClass) {
@@ -66,6 +134,8 @@ function Style(bodyType, driveType, styleID, price, subdivision, marketClass) {
   this.price = price;
   this.subdivision = subdivision;
   this.marketClass = marketClass;
+
+  this.vObj = []
 }
 
 function Catalog(year, make, model, styles) {
@@ -73,4 +143,22 @@ function Catalog(year, make, model, styles) {
     this.make = make;
     this.model = model;
     this.styles = styles;
+
+    this.vObj = [year, make, model, styles];
+    return vObj;
+}
+
+function toggleTable() {
+  var toggle1 = document.getElementById("catalogresponse");
+  var toggle2 = document.getElementById("cr");
+    if (toggle1.style.display == "none") {
+        toggle1.style.display = "block";
+        toggle2.style.display = "block";
+        generateCatalog();
+    } else {
+        toggle1.style.display = "none";
+        toggle2.style.display = "none";
+        clearTable(toggle1);
+        clearTable(toggle2);
+    }
 }
