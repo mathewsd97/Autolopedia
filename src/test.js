8204d9b25f197ed7ddfb7b007b45a7a7886d38b4
Mevin Mathew <mevin@dyn129-100-132-204.eduroam.uwo.ca>
2018-11-24 21:24:41 -0500

    updated global vars outside

--- src/test.js
+++ src/test.js
@@ -1,3 +1,6 @@
+var vin, year, make, model, body_type, drive_type, coi, styleid, price, engine_type, fuel_type, hp, engine_size, fe_cty, fe_hwy, fuel_cap, transmission, tire_size, interior, vObj, compressed;
+
+
 
 function callApi() {
    var vincode = $("#vincode").val();
@@ -6,54 +9,54 @@ function callApi() {
    $.ajax({
        url: url,
        type: 'GET',
-       success: function(results){
-        //    var success_code = obj['S:Envelope']['S:Body[0]'].VehicleDescription[0].responseStatus[0].$.responseCode
-        //    console.log(success_code);
+       success: function(results) {
+    //    var success_code = obj['S:Envelope']['S:Body[0]'].VehicleDescription[0].responseStatus[0].$.responseCode
+    //    console.log(success_code);
 
-           var obj = JSON.parse(JSON.stringify(results));
-           console.log(obj);
-           var vin = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.vin;
-           console.log(vin);
-           var year = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.modelYear;
-           console.log(year);
-           var make = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.division;
-           console.log(make);
-           var model = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.modelName;
-           console.log(model);
-           var body_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.styleName;
-           console.log(body_type);
-           var drive_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.drivingWheels;
-           console.log(drive_type);
-           var coi = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].WorldManufacturerIdentifier[0]
-           console.log(coi);
-           var styleid = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[0].$.id
-           console.log(styleid);
-           var price = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[0].basePrice[0].$.msrp
-           console.log(price);
-           var engine_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].engineType[0]['_']
-           console.log(engine_type);
-           var fuel_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelType[0]['_']
-           console.log(fuel_type);
-           var hp = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].horsepower[0].$.value
-           console.log(hp);
-           var engine_size = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].displacement[0].value[0]['_'] + " " + obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].displacement[0].value[0].$.unit;
-           console.log(engine_size);
-           var fe_cty = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelEconomy[0].city[0].$.low
-           console.log(fe_cty);
-           var fe_hwy = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelEconomy[0].hwy[0].$.low
-           console.log(fe_hwy);
-           var fuel_cap = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelCapacity[0].$.low + obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelCapacity[0].$.unit;
-           console.log(fuel_cap);
-           var transmission = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[3].description[0];
-           console.log(transmission);
-           var tire_size = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[8].description[0];
-           console.log(tire_size);
-           var interior = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[25].description[0];
-           console.log(interior);
+            obj = JSON.parse(JSON.stringify(results));
+            console.log(obj);
+            vin = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.vin;
+            console.log(vin);
+            year = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.modelYear;
+            console.log(year);
+            make = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.division;
+            console.log(make);
+            model = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.modelName;
+            console.log(model);
+            body_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.styleName;
+            console.log(body_type);
+            drive_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].$.drivingWheels;
+            console.log(drive_type);
+            coi = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].vinDescription[0].WorldManufacturerIdentifier[0]
+            console.log(coi);
+            styleid = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[0].$.id
+            console.log(styleid);
+            price = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].style[0].basePrice[0].$.msrp
+            console.log(price);
+            engine_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].engineType[0]['_']
+            console.log(engine_type);
+            fuel_type = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelType[0]['_']
+            console.log(fuel_type);
+            hp = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].horsepower[0].$.value
+            console.log(hp);
+            engine_size = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].displacement[0].value[0]['_'] + " " + obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].displacement[0].value[0].$.unit;
+            console.log(engine_size);
+            fe_cty = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelEconomy[0].city[0].$.low
+            console.log(fe_cty);
+            fe_hwy = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelEconomy[0].hwy[0].$.low
+            console.log(fe_hwy);
+            fuel_cap = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelCapacity[0].$.low + obj['S:Envelope']['S:Body'][0].VehicleDescription[0].engine[0].fuelCapacity[0].$.unit;
+            console.log(fuel_cap);
+            transmission = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[3].description[0];
+            console.log(transmission);
+            tire_size = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[8].description[0];
+            console.log(tire_size);
+            interior = obj['S:Envelope']['S:Body'][0].VehicleDescription[0].standard[25].description[0];
+            console.log(interior);
 
-           var vObj = ADS(vin,year,make, model, body_type, drive_type, coi, styleid, price, engine_type, fuel_type, hp, engine_size, fe_cty, fe_hwy, fuel_cap, transmission, tire_size, interior);
+            vObj = ADS(vin,year,make, model, body_type, drive_type, coi, styleid, price, engine_type, fuel_type, hp, engine_size, fe_cty, fe_hwy, fuel_cap, transmission, tire_size, interior);
             
-           var compressed = JSON.stringify(vObj);
+            compressed = JSON.stringify(vObj);
 
 
 
